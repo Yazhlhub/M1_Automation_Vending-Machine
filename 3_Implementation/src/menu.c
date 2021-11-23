93ae9c469110a6b71aed5811a67d00c8a4408bed
Yazhlhub <86727098+Yazhlhub@users.noreply.github.com>
2021-11-23 20:03:21 +0530

    Update menu.c

--- 3_Implementation/src/menu.c
+++ 3_Implementation/src/menu.c
@@ -8,3 +8,33 @@
  * @copyright Copyright (c) 2021
  * 
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+int menu()
+    {
+  int selection;
+
+
+    printf("---------------------- \n");
+
+
+    printf("MENU: ");
+    printf("\n\n");
+    printf("1. Coca-Cola");            printf("\t\t\t£1.00\n");
+    printf("2. Sprite");               printf("\t\t\t£1.30\n");
+    printf("3. Fanta");                printf("\t\t\t£1.20\n");
+    printf("4. Dr. Pepper");           printf("\t\t\t£1.50\n");
+    printf("5. Quit");
+    printf("\n\n");
+
+
+    printf("Please enter your selection: ");
+    scanf("%d",&selection);
+
+
+    return selection;
+
+
+    }
