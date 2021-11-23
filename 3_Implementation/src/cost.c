15fbfe1e10210e7e022448a2f203fbe3dd187047
Yazhlhub <86727098+Yazhlhub@users.noreply.github.com>
2021-11-23 19:52:49 +0530

    Update cost.c

--- 3_Implementation/src/cost.c
+++ 3_Implementation/src/cost.c
@@ -8,3 +8,29 @@
  * @copyright Copyright (c) 2021
  * 
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+float cost(int selection)
+ {
+  float price = 0;
+
+
+  switch(selection)
+  {
+     case 1:
+       price = 1.00;
+       break;
+     case 2:
+       price = 1.30;
+       break;
+     case 3:
+       price = 1.20;
+       break;
+    case 4:
+       price = 1.50;
+       break;
+   }
+   return price;
+ }
