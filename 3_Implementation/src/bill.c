d15d47d9dac56e1a49f2e951e81f1220b81dc7a6
Yazhlhub <86727098+Yazhlhub@users.noreply.github.com>
2021-11-23 19:50:14 +0530

    Update bill.c

--- 3_Implementation/src/bill.c
+++ 3_Implementation/src/bill.c
@@ -8,3 +8,12 @@
  * @copyright Copyright (c) 2021
  * 
  */
+#include <stdio.h>
+    #include <stdlib.h>
+    #include <conio.h>
+    #include <time.h>
+void bill(float billmoney)
+   {
+       printf("Total amount to be payed is: %.2f\n\n\n",billmoney);
+       return;
+   }
