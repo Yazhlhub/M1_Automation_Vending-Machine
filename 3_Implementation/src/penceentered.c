cb5b6f56d31ab84f709cdd4c52777eedfc1b08b7
Yazhlhub <86727098+Yazhlhub@users.noreply.github.com>
2021-11-23 20:04:46 +0530

    Update penceentered.c

--- 3_Implementation/src/penceentered.c
+++ 3_Implementation/src/penceentered.c
@@ -8,3 +8,17 @@
  * @copyright Copyright (c) 2021
  * 
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+int penceentered()
+   {
+        int pence;
+        printf("Enter your money: \n");
+        printf("(5p 10p 20p 50p 100p)\n");
+        scanf("%d",&pence);
+
+
+       return pence;
+   }
