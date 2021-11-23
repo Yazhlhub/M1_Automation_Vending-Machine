01c06da3c044a98afe850554035dcc66d226ff40
Yazhlhub <86727098+Yazhlhub@users.noreply.github.com>
2021-11-23 20:02:11 +0530

    Update drinktotal.c

--- 3_Implementation/src/drinktotal.c
+++ 3_Implementation/src/drinktotal.c
@@ -8,3 +8,31 @@
  * @copyright Copyright (c) 2021
  * 
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+void drinktotal(int selection,int amount)
+    {
+      switch(selection)
+      {
+      case 1:
+         printf("\n\n\t\t\tYou have selected %d Coca-Cola\n",amount);
+         printf("\t\t\t----------------------------\n");
+         break;
+      case 2:
+         printf("\n\n\t\t\tYou have chosen %d Sprite\n",amount);
+         printf("\t\t\t----------------------------\n");
+         break;
+      case 3:
+         printf("\n\n\t\t\tYou have chosen %d Fanta\n",amount);
+         printf("\t\t\t----------------------------\n");
+         break;
+      case 4:
+         printf("\n\n\t\t\tYou have chosen %d DR Pepper\n",amount);
+         printf("\t\t\t----------------------------\n");
+         break;
+      default:
+         printf("Invalid input!\n");
+         break;
+      }
